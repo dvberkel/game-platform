121e85a5a448204c9731751b0e6a058793ca2687
dvberkel <daan.vanberkel@nedap.com>
2013-04-12 15:37:06 +0200

    create skeletal PixelViewer

---
+++ spec/tools/pixel_spec.js
@@ -0,0 +1,18 @@
+/*globals describe:false, it:false, expect:false, platform: false*/
+"use strict";
+
+describe("platform tools namespace", function(){
+    it("should exist", function(){
+	expect(platform.tools).toBeDefined();
+    });
+});
+
+describe("platform.tools.PixelViewer", function(){
+    it("should exist", function(){
+	expect(platform.tools.PixelViewer).toBeDefined();
+    });
+
+    it("should be a constructor", function(){
+	expect(platform.tools.PixelViewer instanceof Function).toBeTruthy();
+    });
+});
---
+++ src/tools/pixel.js
@@ -0,0 +1,9 @@
+/*global platform:true*/
+(function(platform){
+    platform.tools = platform.tools || {};
+
+    var PixelViewer = function(){};
+
+    var tools = platform.tools;
+    tools.PixelViewer = PixelViewer;
+})(platform);
