f2f239b6bf1d200de04a8b05cd8a2f8cd0a3ed5b
dvberkel <daan.vanberkel@nedap.com>
2013-04-12 14:30:13 +0200

    add jasmine task

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,12 @@ module.exports = function(grunt) {
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
       ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
     // Task configuration.
+    jasmine: {
+      src: ['src/**/*.js'],
+      options: {
+        specs: 'spec/**/*_spec.js'
+      }
+    },
     concat: {
       options: {
         banner: '<%= banner %>',
@@ -64,8 +70,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'jasmine', 'concat', 'uglify']);
 
 };
